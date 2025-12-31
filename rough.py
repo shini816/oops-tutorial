73f026a68027d7eee9ab3ed9449507957f69fd0f
JYO <opbotexp06@gmail.com>
2025-12-31 09:49:35 +0530

    covered till chatbox project full

--- rough.py
+++ rough.py
@@ -1,9 +1,13 @@
-lst = [1,2,3]
-my_str = "mlops playlist"
-my_int = 155
+# lst = [1,2,3]
+# my_str = "mlops playlist"
+# my_int = 155
 
-print(type(lst))
-print(type(my_str))
-print(type(my_int))
+# print(type(lst))
+# print(type(my_str))
+# print(type(my_int))
 
-# any data structure/type under python is a class
+# # any data structure/type under python is a class
+
+from opps_proj import chatbox
+
+user1 = chatbox()
